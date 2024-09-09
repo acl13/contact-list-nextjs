4ca758a891677e180760a737dfcb6f3618b93baf
acl13 <128934431+acl13@users.noreply.github.com>
2024-09-09 17:06:53 -0400

    display contacts as list (names only)

--- app/contacts/page.js
+++ app/contacts/page.js
@@ -1,9 +1,19 @@
+"use client";
 import Link from "next/link";
+import { ContactAPI } from "../data/contactAPI";
 
 export default function ContactPage() {
+  const contacts = ContactAPI.contacts;
   return (
     <div>
       <h1>All Contacts</h1>
+      <ul>
+        {contacts.map((contact) => (
+          <li key={contact.id}>
+            <Link href={`/contacts/${contact.id}`}>{contact.name}</Link>
+          </li>
+        ))}
+      </ul>
       <button>
         <Link href="/contacts/new">Add Contact</Link>
       </button>
