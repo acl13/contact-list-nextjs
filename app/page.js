bfa4f333b50dc2572b60a004f561a02b91f5cb52
acl13 <128934431+acl13@users.noreply.github.com>
2024-09-09 17:05:11 -0400

    display ContactPage on main page

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import ContactPage from "./contacts/page";
 import styles from "./page.module.css";
 import Link from "next/link";
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Welcome to Your Contact List</h1>
+      <ContactPage />
       <Link href="/contacts">Click here to view your contacts</Link>
     </main>
   );
