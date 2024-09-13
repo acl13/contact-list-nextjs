51c4e507058aa5b6ca206b2a8a0dc338940d6b57
acl13 <128934431+acl13@users.noreply.github.com>
2024-09-13 13:36:39 -0400

    update input state as user types in field

--- app/contacts/new/page.js
+++ app/contacts/new/page.js
@@ -1,9 +1,27 @@
 "use client";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import Link from "next/link";
+
+function useFormInput(initialValue) {
+  const [value, setValue] = useState(initialValue);
+
+  const onChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  return {
+    value,
+    onChange,
+  };
+}
 
 export default function NewContact() {
   const router = useRouter();
+  const name = useFormInput("");
+  const email = useFormInput("");
+  const phone = useFormInput("");
+  const imgUrl = useFormInput("");
 
   const handleSubmit = () => {
     // add contact to contacts list
@@ -14,18 +32,37 @@ export default function NewContact() {
     <div className="content-container">
       <h1>Add Contact</h1>
       <form className="form">
-        <label>Name</label>
-        <input type="text"></input>
+        <label>Name: </label>
+        <input type="text" placeholder="name" required {...name}></input>
 
         <br />
 
         <label>Email</label>
-        <input type="text"></input>
+        <input
+          type="text"
+          placeholder="name@example.com"
+          required
+          {...email}
+        ></input>
 
         <br />
 
         <label>Phone</label>
-        <input type="text"></input>
+        <input
+          type="text"
+          placeholder="(1)000-000-0000"
+          required
+          {...phone}
+        ></input>
+
+        <br />
+        <label>Image URL</label>
+        <input
+          type="text"
+          placeholder="https://www.example.com"
+          required
+          {...imgUrl}
+        ></input>
         <button type="button" onClick={handleSubmit}>
           Submit
         </button>
