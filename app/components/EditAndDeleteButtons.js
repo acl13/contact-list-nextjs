a55fea69f30ea01883f4bdc8cd2850ab39b573f6
acl13 <128934431+acl13@users.noreply.github.com>
2024-09-16 14:06:49 -0400

    ask user to confirm delete

--- app/components/EditAndDeleteButtons.js
+++ app/components/EditAndDeleteButtons.js
@@ -1,20 +1,40 @@
+import { useState } from "react";
 import { FaRegEdit } from "react-icons/fa";
 import { FaRegTrashCan } from "react-icons/fa6";
 
 export default function EditAndDeleteButtons({ contact, removeContact }) {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
   return (
     <>
       <button className="margin-5 padding-2">
         <FaRegEdit size="1.5em" />
       </button>
-      <button
-        className="margin-5 padding-2"
-        onClick={() => {
-          removeContact(contact);
-        }}
-      >
+      <button className="margin-5 padding-2" onClick={openModal}>
         <FaRegTrashCan size="1.5em" />
       </button>
+      <dialog className="content-container text-center" open={modalOpen}>
+        <p>Are you sure you want to delete this contact?</p>
+        <button className="margin-5 padding-2" onClick={closeModal}>
+          Cancel
+        </button>
+        <button
+          className="margin-5 padding-2"
+          onClick={() => {
+            removeContact(contact);
+          }}
+        >
+          Confirm delete
+        </button>
+      </dialog>
     </>
   );
 }
