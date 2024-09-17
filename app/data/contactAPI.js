01ad5a2da9f842f80ed1f9646a767ff067a32aee
acl13 <128934431+acl13@users.noreply.github.com>
2024-09-17 15:44:56 -0400

    remove dummy data

--- app/data/contactAPI.js
+++ app/data/contactAPI.js
@@ -4,44 +4,7 @@
  **/
 
 export const ContactAPI = {
-  contacts: [
-    {
-      id: 70219571,
-      name: "Albert Einstein 1",
-      email: "aeinstein@example.com",
-      phone_number: "11111111111",
-      image_url:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Einstein_1921_by_F_Schmutzer_-_restoration.jpg/1280px-Einstein_1921_by_F_Schmutzer_-_restoration.jpg",
-    },
-    {
-      id: 70219572,
-      name: "Bob Regular",
-      email: "bregular@example.com",
-      phone_number: "11234567890",
-      image_url: "url",
-    },
-    {
-      id: 70219573,
-      name: "Cats Mcgee",
-      email: "cats@example.com",
-      phone_number: "13333333333",
-      image_url: "url",
-    },
-    {
-      id: 70219574,
-      name: "Fred Flinstone",
-      email: "fflinstone@example.com",
-      phone_number: "14444444444",
-      image_url: "url",
-    },
-    {
-      id: 70219575,
-      name: "Poppy Seed",
-      email: "poppys@example.com",
-      phone_number: "15555555555",
-      image_url: "url",
-    },
-  ],
+  contacts: [],
   all: function () {
     return this.contacts;
   },
